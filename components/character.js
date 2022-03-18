1e461bb6a217ebf04bd10ed3bda1f3895776743d
juan david (iron-mind) <davidtovarmontoya@gmail.com>
2022-03-18 18:11:17 +0100

    expo-updates for deploy

--- components/character.js
+++ components/character.js
@@ -1,36 +1,38 @@
-import * as React from 'react';
-import { Text, View,  Image ,StyleSheet, TouchableOpacity} from 'react-native';
+import * as React from "react";
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
 
-    backgroundColor: '#46237A',
+    backgroundColor: "#46237A",
 
     borderWidth: 3,
-    borderColor:"white",
-    width:'100%',
-    maxHeight:80,
-    alignItems: 'center',
+    borderColor: "white",
+    width: "100%",
+    maxHeight: 80,
+    alignItems: "center",
     // justifyContent: 'center',
-    padding:3,
-    flexDirection: 'row'
+    padding: 3,
+    flexDirection: "row",
   },
-  previewImage:{
-    height:70,
-    width:90,
-    margin:4
-  }
+  previewImage: {
+    height: 70,
+    width: 90,
+    margin: 4,
+  },
 });
-export default function CharacterCard({image,id, name,navigation}) {
-
+export default function CharacterCard({ image, id, name, navigation }) {
   return (
-
-    <TouchableOpacity style={styles.container} onPress={()=>{ navigation.navigate("Detail",{name,image,id})}}>
-			<Image
-        style={styles.previewImage}
-				source={{uri:image}}
-			/>
-      <Text style={{color: 'white'}}name={name}>{name}</Text>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => {
+        navigation.navigate("Detail", { name, image, id });
+      }}
+    >
+      <Image style={styles.previewImage} source={{ uri: image }} />
+      <Text style={{ color: "white" }} name={name}>
+        {name}
+      </Text>
     </TouchableOpacity>
   );
 }
