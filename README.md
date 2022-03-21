045a459daab862394381b5506b2cd94791e05958
juan david (iron-mind) <davidtovarmontoya@gmail.com>
2022-03-21 16:00:26 +0100

    infinite scroll

--- README.md
+++ README.md
@@ -1,27 +1,32 @@
-# MarvelApp - React Native MarvelApp
+# MarvelApp - React Native App
 
 ### Know comics related to any character of Marvel. Go ahead. Look and enjoy
+
+Download android .apk here https://drive.google.com/file/d/1AaxGPq-aF8t8Kr6_5rNuv6I3ZdHC0VuC/view?usp=sharing
 
 Video. Click here [![Watch the video](./imgs/video.jpg)](https://youtu.be/C8HZUGwKVuU)
 https://youtu.be/C8HZUGwKVuU
 
 ## Home
+
 <p align="center">
   <img src="./imgs/home.jpg" style="width: 70%"/>
 </p>
 
 ## Detail
+
 <p align="center">
   <img src="./imgs/detail.jpg" style="width: 70%"/>
 </p>
 
 ## Search
+
 <p align="center">
   <img src="./imgs/search.jpg" style="width: 70%"/>
 </p>
 
-
 ## A-Bomb
+
 <p align="center">
   <img src="./imgs/abomb.png" style="width: 80%"/>
 </p>
--- components/home.js
+++ components/home.js
@@ -1,69 +1,98 @@
-import * as React from 'react';
-import { Text, View , ActivityIndicator,FlatList} from 'react-native';
-import CharacterCard from './character';
-import { Searchbar } from 'react-native-paper';
-import apiParams from '../config.js';
-import axios from 'axios';
+import * as React from "react";
+import { Text, View, ActivityIndicator, FlatList } from "react-native";
+import CharacterCard from "./character";
+import { Searchbar } from "react-native-paper";
+import apiParams from "../config.js";
+import axios from "axios";
 
 export default function Home(props) {
   const [isLoading, setLoading] = React.useState(true);
   const [data, setData] = React.useState([]);
+  const [dataInPage, setDataInPage] = React.useState([]);
   const { ts, apikey, hash, baseURL } = apiParams;
-const [search, setSearch] = React.useState('');
+  const [search, setSearch] = React.useState("");
+  const [currentPage, setCurrentPage] = React.useState(1);
+
   React.useEffect(() => {
-    axios.get(`${baseURL}/v1/public/characters`, {
-      params: {
-        ts,
-        apikey,
-        hash
-      }
-    })
-      .then(response => setData(response.data.data.results))
-      .catch(error => console.error(error))
-      .finally(() => {setLoading(false)});
+    axios
+      .get(`${baseURL}/v1/public/characters`, {
+        params: {
+          ts,
+          apikey,
+          hash,
+          limit: 100, //more characters for infinity escroll
+        },
+      })
+      .then((response) => {
+        setData(response.data.data.results);
+        setDataInPage(
+          response.data.data.results.slice(0, 10 * currentPage - 1)
+        );
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
+  const renderMoreItems = () => {
+    setDataInPage(data.slice(0, 10 * (currentPage + 1) - 1));
+    setCurrentPage(currentPage + 1);
+  };
+
+  const renderLoader = () => {
+    return (
+      <View style={{ margin: 20, padding: 20 }}>
+        <ActivityIndicator size="large" color="#aaa" />
+      </View>
+    );
+  };
+
   function searchCharacter() {
-if(search) {
- setLoading(true);
- axios.get(`${baseURL}/v1/public/characters`, {
-   params: {
-     ts,
-     apikey,
-     hash,
-     nameStartsWith: search
-   }
- })
-   .then(response => setData(response.data.data.results))
-   .catch(error => console.error(error))
-   .finally(() => setLoading(false));
-}
-}
+    if (search) {
+      setLoading(true);
+      axios
+        .get(`${baseURL}/v1/public/characters`, {
+          params: {
+            ts,
+            apikey,
+            hash,
+            nameStartsWith: search,
+          },
+        })
+        .then((response) => setData(response.data.data.results))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }
+  }
   return (
-    <View >
-    <Searchbar
-placeholder="Search for character..."
-onChangeText={value => setSearch(value)}
-value={search}
-onIconPress={searchCharacter}
-onSubmitEditing={searchCharacter}
-/>
-      {isLoading
-        ? <ActivityIndicator size="large" color="#00ff00" />
-        : (
-          <FlatList
-            data={data}
-            keyExtractor={({ id }) => id.toString()}
-            renderItem={({ item }) => (
-              <CharacterCard
+    <View>
+      <Searchbar
+        placeholder="Search for character..."
+        onChangeText={(value) => setSearch(value)}
+        value={search}
+        onIconPress={searchCharacter}
+        onSubmitEditing={searchCharacter}
+      />
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#00ff00" />
+      ) : (
+        <FlatList
+          data={dataInPage}
+          keyExtractor={({ id }) => id.toString()}
+          renderItem={({ item }) => (
+            <CharacterCard
               {...props}
-                id={item.id}
-                image={`${item?.thumbnail?.path}.${item?.thumbnail.extension}`}
-                name={item.name} />
-            )}
-          />
-        )
-      }
+              id={item.id}
+              image={`${item?.thumbnail?.path}.${item?.thumbnail.extension}`}
+              name={item.name}
+            />
+          )}
+          ListFooterComponent={renderLoader}
+          onEndReached={renderMoreItems}
+          onEndReachedThreshold={0}
+        />
+      )}
     </View>
   );
 }
